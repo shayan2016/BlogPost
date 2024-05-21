884270e9a4f02ef97083c10d488107a797e90a6f
shayan2016 <shayansamanta2016@gmail.com>
2024-05-21 12:10:15 +0530

    new files added

---
+++ src/store/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    state : false,
+    userData: null
+}
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login : (state, action) =>{
+            state.status = true;
+            state.userData = action.payload.useData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+export const {login, logout} =authSlice.actions;
+
+export default authSlice.reducer;
